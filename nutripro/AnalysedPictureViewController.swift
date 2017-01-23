5d3a74887dfe6643a786da2939432a1b5578d6b1
Rafael Camargos Santos <rafael.appsimples@gmail.com>
2017-01-23 13:09:34 -0200

    setup table view

--- nutripro/AnalysedPictureViewController.swift
+++ nutripro/AnalysedPictureViewController.swift
@@ -8,12 +8,22 @@
 
 import UIKit
 
-class AnalysedPictureViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class AnalysedPictureViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITableViewDelegate, UITableViewDataSource {
     
     let imagePicker = UIImagePickerController()
+    @IBOutlet weak var tableView: UITableView!
+    
+    let ingredients = 3;
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "analysedPicturePhotoCell")
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "analyzedPictureItemCell")
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "analysedPictureButton")
         
         openCamera();
         
@@ -22,6 +32,40 @@ class AnalysedPictureViewController: UITableViewController, UIImagePickerControl
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView:UITableView, numberOfRowsInSection section:Int) -> Int
+    {
+        if(section == 1 || section == 3) {
+            return 1
+        }
+        else {
+            return 3
+        }
+    }
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int
+    {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if(indexPath.section == 1){
+            let cell = tableView.dequeueReusableCell(withIdentifier: "analysedPicturePhotoCell")! as UITableViewCell
+//            cell.imageView?.image = imageName
+            return cell
+        }
+        else if(indexPath.section == 2){
+            let cell = tableView.dequeueReusableCell(withIdentifier: "analyzedPictureItemCell")! as UITableViewCell
+            return cell
+        }
+        else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "analysedPictureButton")! as UITableViewCell
+            return cell
+        }
+
     }
     
     func openCamera() {
@@ -48,6 +92,13 @@ class AnalysedPictureViewController: UITableViewController, UIImagePickerControl
     func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
 //        imagePicked.image = image
 //        self.dismissViewControllerAnimated(true, completion: nil);
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath as IndexPath, animated: true)
+        
+        let row = indexPath.row
+//        print(swiftBlogs[row])
     }
 
     /*
